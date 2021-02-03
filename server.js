68ee69aeeeebd9bc919b950b33a75afeb4c86074
anisioaleixo <anisioaleixo@yahoo.com.br>
2021-02-03 17:23:56 -0300

    Correção de caminho

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const https = require("https");
 const express = require("express");
-const path = require('path')
 
 // Instância express
 const app = express();
@@ -11,8 +10,8 @@ app.get("/", (req, res) => {
 
 // Carrega o certificado e a key necessários para a configuração.
 const options = {
-  key: fs.readFileSync(path.resolve(__dirname,"certificado","server.key")),
-  cert: fs.readFileSync(path.resolve(__dirname,"certificado","server.cert"))
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert")
 };
 
 // Cria a instância do server e escuta na porta 3000
